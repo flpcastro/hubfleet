1b239e2aac003eca9c54a89d86badf74b41f8052
Felipe <flpc.dev@gmail.com>
2023-09-15 17:59:26 -0300

    feat: implement styled-components provider and create global theme

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { styles } from './src/screens/SignIn/styles';
 import { SignIn } from './src/screens/SignIn';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/theme';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SignIn />
-    </>
+    </ThemeProvider>
   );
 }
