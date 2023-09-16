f3cbecf4d922d798b93f395304a983aec28d3154
Felipe <flpc.dev@gmail.com>
2023-09-16 04:40:28 -0300

    refactor: add semicolons

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import { StatusBar } from 'react-native'
-import { ThemeProvider } from 'styled-components/native'
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { SignIn } from './src/screens/SignIn'
-import { Loading } from './src/components/Loading'
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
-import theme from './src/theme'
+import theme from './src/theme';
 
 export default function App() {
-	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
 	if(!fontsLoaded) {
 		return <Loading />
