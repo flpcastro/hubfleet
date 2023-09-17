b71043c2095226a04583f1fd60c658734edb6b10
Felipe <flpc.dev@gmail.com>
2023-09-17 04:11:00 -0300

    style: add padding top with safeareacontext

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -5,17 +5,21 @@ import { Power } from 'phosphor-react-native';
 
 import * as S from './styles';
 import theme from '../../theme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export function HomeHeader() {
   const user = useUser();
   const app = useApp();
+  const insets = useSafeAreaInsets();
+
+  const paddingTop = insets.top + 32; 
 
   function handleLogout() {
     app.currentUser?.logOut();
   }
 
   return (
-    <S.Container>
+    <S.Container style={{ paddingTop }}>
       <S.Picture 
         source={{ uri: user.profile.pictureUrl }}
         placeholder="L184i9kCbIof00ayjZay~qj[ayj@"
