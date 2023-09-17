329629cee5f0bc310fba3db1fd69785ddca624eb
Felipe <flpc.dev@gmail.com>
2023-09-17 03:08:54 -0300

    feat: create login flux

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from 'react';
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env';
+
+import * as S from './styles';
+
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
 
 import backgroundImg from '../../assets/background.png';
 import { Button } from '../../components/Button';
-import * as S from './styles';
-import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env';
 import { Alert } from 'react-native';
+
+import { Realm, useApp } from '@realm/react';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export function SignIn() {
 	const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+	const app = useApp();
 
 	const [_, response, googleSignIn] = Google.useAuthRequest({
 		androidClientId: ANDROID_CLIENT_ID,
@@ -32,7 +38,12 @@ export function SignIn() {
 	useEffect(() => {
 		if(response?.type === 'success') {
 			if(response.authentication?.idToken) {
-
+				const credentials = Realm.Credentials.jwt(response.authentication.idToken);
+				app.logIn(credentials).catch(error => {
+					console.log(error);
+					Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta Google');
+					setIsAuthenticating(false);
+				})
 			} else {
 				Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta Google');
 				setIsAuthenticating(false);
