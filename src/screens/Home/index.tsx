219c17ceb613b85f0f37b185453f6138b96adc84
Felipe <flpc.dev@gmail.com>
2023-09-19 22:46:19 -0300

    feat: implement navigation in carstatus to departure

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,23 @@
+import { useNavigation } from '@react-navigation/native';
 import { CarStatus } from '../../components/CarStatus';
 import { HomeHeader } from '../../components/HomeHeader';
 import * as S from './styles';
 
 export function Home() {
+  const { navigate } = useNavigation();
+
+  function handleRegisterMovement() {
+    navigate('departure');
+  }
+
   return (
     <S.Container>
       <HomeHeader />
 
       <S.Content>
-        <CarStatus />
+        <CarStatus 
+          onPress={handleRegisterMovement}
+        />
       </S.Content>
     </S.Container>
   );
