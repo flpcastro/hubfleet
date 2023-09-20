24b7ff7adf74a747c656bc5549ae420942aa86b5
Felipe <flpc.dev@gmail.com>
2023-09-19 23:19:33 -0300

    feat: adjust inputs to better user experience

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,9 +1,18 @@
+import { useRef } from 'react';
+import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { LicensePlateInput } from '../../components/LicensePlateInput';
 import { TextAreaInput } from '../../components/TextAreaInput';
 import * as S from './styles';
+import { TextInput } from 'react-native';
 
 export function Departure() {
+  const descriptionRef = useRef<TextInput>(null);
+
+  function handleDepartureRegister() {
+    console.log('ok')
+  }
+
   return (
     <S.Container>
       <Header 
@@ -14,11 +23,22 @@ export function Departure() {
         <LicensePlateInput 
           label='Placa do veículo'
           placeholder='BRA1234'
+          onSubmitEditing={() => descriptionRef.current?.focus()}
+          returnKeyType='next'
         />
 
         <TextAreaInput 
+          ref={descriptionRef}
           label='Finalidade'
           placeholder='Vou utilizar o veículo para...'
+          onSubmitEditing={handleDepartureRegister}
+          returnKeyType='send'
+          blurOnSubmit
+        />
+
+        <Button 
+          title='Registrar Saída'
+          onPress={handleDepartureRegister}
         />
       </S.Content>
     </S.Container>
