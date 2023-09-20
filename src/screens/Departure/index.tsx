f99234cae1217beb6e0607609bdc2dbbc90c732a
Felipe <flpc.dev@gmail.com>
2023-09-19 23:06:25 -0300

    feat: create text area input component

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header';
 import { LicensePlateInput } from '../../components/LicensePlateInput';
+import { TextAreaInput } from '../../components/TextAreaInput';
 import * as S from './styles';
 
 export function Departure() {
@@ -13,6 +14,11 @@ export function Departure() {
         <LicensePlateInput 
           label='Placa do veículo'
           placeholder='BRA1234'
+        />
+
+        <TextAreaInput 
+          label='Finalidade'
+          placeholder='Vou utilizar o veículo para...'
         />
       </S.Content>
     </S.Container>
