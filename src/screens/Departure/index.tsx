d0d3a5b02a25530d10ceb80e3bea7717091d67a6
Felipe <flpc.dev@gmail.com>
2023-09-19 22:45:49 -0300

    feat: create departure screen

---
+++ src/screens/Departure/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from '../../components/Header';
+import * as S from './styles';
+
+export function Departure() {
+  return (
+    <S.Container>
+      <Header 
+        title='Saída'
+      />
+    </S.Container>
+  );
+}
