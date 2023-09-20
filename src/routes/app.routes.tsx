d18b5cb166183cdc2bd071c29eac4fddae3b4f56
Felipe <flpc.dev@gmail.com>
2023-09-19 22:30:29 -0300

    feat: add departure screen in routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from '../screens/Home';
+import { Departure } from '../screens/Departure';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -7,8 +8,13 @@ export function AppRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen
-        name="Home" 
+        name="home" 
         component={Home}
+      />
+
+      <Screen
+        name="departure" 
+        component={Departure}
       />
     </Navigator>
   )
