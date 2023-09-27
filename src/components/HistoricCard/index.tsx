0858dd9490a0d7e1fab5b069b0e7dc27fa1268f7
Felipe <flpc.dev@gmail.com>
2023-09-26 23:41:08 -0300

    feat: create historic card component

---
+++ src/components/HistoricCard/index.tsx
@@ -0,0 +1,46 @@
+import { TouchableOpacityProps } from 'react-native';
+import * as S from './styles';
+import { Check, ClockClockwise } from 'phosphor-react-native';
+import { useTheme } from 'styled-components/native';
+
+export type HistoricCardProps = {
+  licensePlate: string;
+  created: string;
+  isSync: boolean;
+};
+
+type Props = TouchableOpacityProps & {
+  data: HistoricCardProps
+}
+
+export function HistoricCard({ data, ...rest }: Props) {
+  const { COLORS } = useTheme();
+
+  return (
+    <S.Container {...rest}>
+      <S.Info>
+        <S.LicensePlate>
+          {data.licensePlate}
+        </S.LicensePlate>
+
+        <S.Departure>
+          {data.created}
+        </S.Departure>
+      </S.Info>
+
+      {
+        data.isSync 
+        ? 
+          <Check 
+            size={24}
+            color={COLORS.BRAND_LIGHT}
+          />
+        :
+          <ClockClockwise 
+            size={24}
+            color={COLORS.GRAY_400}
+          />
+      }
+    </S.Container>
+  );
+}
