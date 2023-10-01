5afa6db5b74cb97ca0f6f46a2e473acd3b0cce11
Felipe <flpc.dev@gmail.com>
2023-10-01 04:04:23 -0300

    feat: show sync percentage

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -11,10 +11,13 @@ import dayjs from 'dayjs';
 import { useUser } from '@realm/react';
 import { getLastSyncTimestamp, saveLastSyncTimestamp } from '../../libs/asyncStorage/syncStorage';
 import Toast from 'react-native-toast-message';
+import { TopMessage } from '../../components/TopMessage';
+import { CloudArrowUp } from 'phosphor-react-native';
 
 export function Home() {
   const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
   const [vehicleHistoric, setVehicleHistoric] = useState<HistoricCardProps[]>([]);
+  const [percentageToSync, setPercentageToSync] = useState<string | null>(null);
 
   const { navigate } = useNavigation();
 
@@ -71,12 +74,17 @@ export function Home() {
     
     if(percentage === 100) {
       await saveLastSyncTimestamp();
-      fetchHistoric();
+      await fetchHistoric();
+      setPercentageToSync(null);
 
       Toast.show({
         type: 'info',
         text1: 'Todos os dados estão sincronizados',
       })
+    }
+
+    if(percentage < 100) {
+      setPercentageToSync(`${percentage.toFixed(0)}% sincronizado.`);
     }
   }
 
@@ -122,6 +130,10 @@ export function Home() {
 
   return (
     <S.Container>
+      {
+        percentageToSync && <TopMessage title={percentageToSync} icon={CloudArrowUp} />
+      }
+
       <HomeHeader />
 
       <S.Content>
